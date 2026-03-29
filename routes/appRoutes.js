a6c6e6787b67d55aa28a11dc19a3bac67935868c
Dell <adityaganji889@gmail.com>
2026-03-29 20:00:50 +0530

    latest commit

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { getAllApps, getExistingApp, addNewApp, modifyExistingApp, deleteExistingApp } = require("../controllers/appControllers");
 
-
+//routes for adding apps
 router.get("/getAllApps",getAllApps);
 router.get("/getExistingApp/:id",getExistingApp);
 router.post("/addNewApp",addNewApp);
