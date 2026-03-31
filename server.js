18c2d917d64664625b78fdb620025f8872d897d9
Dell <adityaganji889@gmail.com>
2026-03-31 18:02:29 +0530

    Added Swagger UI

--- server.js
+++ server.js
@@ -59,20 +59,48 @@ app.use("/api/apps",appRoute);
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get("/api-docs.json", (req, res) => {
   res.json(swaggerDocs);
 });
 
+app.get("/api-docs", (req, res) => {
+  // res.json(swaggerDocs);
+  res.setHeader("Content-Type", "text/html");
+  res.end(`
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Swagger</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+
+  <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = function() {
+      SwaggerUIBundle({
+        url: window.location.origin + '/api-docs.json',
+        dom_id: '#swagger-ui'
+      });
+    };
+  </script>
+</body>
+</html>
+  `);
+});
+
 // Serve UI
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(null, {
-    swaggerOptions: {
-      url: "/api-docs.json",
-    },
-  })
-);
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(null, {
+//     swaggerOptions: {
+//       url: "/api-docs.json",
+//     },
+//   })
+// );
 
 app.listen(port,()=>{
     console.log(`Server is running on port : ${port}`);
